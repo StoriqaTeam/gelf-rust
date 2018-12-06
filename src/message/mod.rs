09ab5d59d5228aa3933a8144a64c7e8cc6d6a080
Daniil Kolesnichenko <d.s.kolesnichenko@yandex.ru>
2018-12-06 13:30:47 +0300

    TEST

--- src/message/mod.rs
+++ src/message/mod.rs
@@ -169,8 +169,9 @@ impl<'a> From<&'a log::Record<'a>> for Message<'a> {
     /// Create a `Message` from given `log::LogRecord` including all metadata
     fn from(record: &'a log::Record) -> Message<'a> {
         // Create message with given text and level
-        let short_message = format!("{}", record.args());
-        let short_message = util::hide_passwords(short_message);
+//        let short_message = format!("{}", record.args());
+//        let short_message = util::hide_passwords(short_message);
+        let short_message = "TEST";
 
         let mut msg = Message::new_with_level(
             short_message,
