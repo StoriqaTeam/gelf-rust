adf7f9fb188e2ef1578dc6eca77013f8205422c1
Daniil Kolesnichenko <d.s.kolesnichenko@yandex.ru>
2018-12-07 12:22:10 +0300

    Hide passwords from logs

--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,21 @@
 use libc;
+use regex::Regex;
 
 /// Return the process-id (pid) of the current process
 pub fn pid() -> i32 {
     unsafe { libc::getpid() }
+}
+
+pub fn hide_credentials<'a>(s: String) -> String {
+    let mut res = s;
+    let patterns = vec![
+        (Regex::new("password\\s*:\\s*Some\\s*\\(\\s*\"[^\"]*\"\\s*\\)").unwrap(), "password: Some(\"****\")"),
+        (Regex::new("(?P<n>\"[a-zA-Z0-9_ ]*[pP]assword[a-zA-Z0-9_ ]*\")\\s*:\\s*\"[^\"]*\"").unwrap(), "$n: \"****\"")
+    ];
+
+    for (pattern, replace) in patterns {
+        res = pattern.replace_all(res.as_ref(), replace).to_string();
+    }
+
+    res
 }
